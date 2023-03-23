944531e2e845ad8906b98c7e8a56f1014007146b
Ha Nguyen <vietha.nguyen3110@gmail.com>
2023-03-22 22:32:29 -0700

    Installed tailwind css

--- mini-fe/src/App.js
+++ mini-fe/src/App.js
@@ -4,13 +4,13 @@ import {
   Routes
 } from "react-router-dom";
 import HomePage from './components/HomePage';
-
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<HomePage/>}></Route>
+        <Route path='/' element={<><Navigation/><HomePage/></>}></Route>
       </Routes>
     </div>
   );
