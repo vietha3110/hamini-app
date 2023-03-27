f60c05020047653cc9cc595598a50e08f826d235
Ha Nguyen <vietha.nguyen3110@gmail.com>
2023-03-26 23:26:21 -0700

    Added weather icons & draft UI weather card.

--- mini-fe/src/components/Weather/WeatherCard/index.js
+++ mini-fe/src/components/Weather/WeatherCard/index.js
@@ -1,11 +1,12 @@
+import Cloud from '../../../assets/weather/suncloud.png'
+
 function convertKtoF(temp) {
     return ((temp - 273.15) * (9 / 15) + 32).toFixed(0);
 }
 const WeatherCard = ({ location, main, sys, weather, wind }) => {
-    const temp = convertKtoF(main.temp);
     return (
         <div className="flex w-full justify-center items-center h-96 ">
-            <div className="flex border rounded-md flex-col w-80 h-80 px-2 bg-white text-black">
+            <div className="flex border rounded-md flex-col w-96 h-80 px-2 bg-white text-black">
                 <div className="my-2">
                     <div className="flex flex-row">
                         <div className="w-10 flex justify-center items-center">
@@ -13,23 +14,34 @@ const WeatherCard = ({ location, main, sys, weather, wind }) => {
                                 location_on
                             </span>
                         </div>
-                        <span className="capitalize">{location}</span>
+                        <span className="capitalize font-bold">{location}</span>
                     </div>
                 </div>
                 <div>
                     <div>
-                        Icon weather rain/sun...
+                       Today's Report
                     </div>
                     <div>
-                        {temp} F
-                        <br />
-                        <p>
-                            Feels like {convertKtoF(main.feels_like)} F
-                        </p>
-                        <div>
-                            {weather.description}
+                        <div className='flex justify-center items-center'>
+                            <img
+                                src={Cloud}
+                                alt="cloud"
+                                className='w-24 h-30'
+                            />
+                        </div>
+                        <div className='flex flex-col justify-center items-center'>
+                            <p className='font-bold text-lg'>
+                                {convertKtoF(main.temp)} F
+                            </p>
+                            <p>
+                                Feels like {convertKtoF(main.feels_like)} F
+                            </p>
+                            <div>
+                                {weather.description}
+                            </div>
                         </div>
                     </div>
+                   
                 </div>
                 <div>
                     <div>
